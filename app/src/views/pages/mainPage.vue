788da44496f79da60486e5ca5fa3d16b27f73460
johnny-24 <dos_24@inbox.ru>
2021-11-24 23:20:14 +0500

    vue init

---
+++ app/src/views/pages/mainPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Header />
+    MainPage
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue';
+import todo from '@/api/todo';
+
+export default {
+  name: 'MainPage',
+  components: {
+    Header,
+  },
+  mounted() {
+    todo.fetchAll();
+  },
+};
+</script>
