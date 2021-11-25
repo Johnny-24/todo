73f8b8f1bc3865f362391e36b9cc313b76c0dca2
johnny-24 <dos_24@inbox.ru>
2021-11-26 00:35:14 +0500

    main page and bootstrap

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -1,25 +1,11 @@
 <template>
-  <div class="relative bg-white">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6">
-      <div class="
-        flex
-        justify-between
-        items-center
-        border-b-2
-        border-gray-100
-        py-6
-        md:justify-start
-        md:space-x-10
-      ">
-        <div class="flex justify-start lg:w-0 lg:flex-1">
-          <a href="#">
-            <span class="sr-only">Workflow</span>
-            <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="">
-          </a>
+  <header class="d-flex align-items-center bg-success">
+    <div class="container">
+        <div class="d-flex justify-content-end">
+          <button type="button" class="btn btn-warning">Авторизоваться</button>
         </div>
-      </div>
     </div>
-  </div>
+  </header>
 </template>
 
 <script>
@@ -27,3 +13,9 @@ export default {
   name: 'Header',
 };
 </script>
+
+<style scoped>
+header {
+  height: 50px;
+}
+</style>
--- app/src/views/pages/mainPage.vue
+++ app/src/views/pages/mainPage.vue
@@ -1,7 +1,37 @@
 <template>
   <div>
     <Header />
-    MainPage
+    <div class="container">
+      <h1 class="text-center">TODO</h1>
+      <form class="row mb-5">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6">
+          <label class="visually-hidden" for="specificSizeInputGroupUsername">Todo</label>
+          <div class="input-group">
+            <input
+              type="text"
+              class="form-control"
+              id="specificSizeInputGroupUsername"
+              placeholder="Todo"
+            >
+            <button type="button" class="btn btn-success">Создать</button>
+          </div>
+        </div>
+        <div class="col-sm-3"></div>
+      </form>
+      <div class="row">
+        <div class="col-3"></div>
+        <div class="col-6">
+          <div class="card" style="width: 100%">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <a href="#" class="btn btn-danger">Удалить</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-3"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,3 +49,9 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+h1 {
+  margin: 20px 0;
+}
+</style>
