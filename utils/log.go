2e513374b4ad9aeea248aed70f274185a2a5c056
Евгений Кудрявцев <johnny@llr-vlan87-87.ll.mgn.ru>
2025-06-13 00:36:19 +0500

    add log

--- utils/log.go
+++ utils/log.go
@@ -2,21 +2,44 @@ package utils
 
 import (
 	"fmt"
-	"time"
 	"os"
+	"time"
 )
 func SaveLog () {
+	filePath := "log.txt"
+	var oldData string
+	fileExist := true
+	fileInfo, err := os.Stat(filePath)
+	if err != nil {
+		fileExist = false
+	}
+
+	if fileExist {
+		data, _ := os.ReadFile(filePath)
+		oldData = string(data)
+		fileSize := fileInfo.Size()
+		fmt.Println(fileSize > 248)
+		if fileSize > 248 {
+			oldData = ""
+			err := os.Remove(filePath)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+
 	today := time.Now()
-	str := "Последняя дата запуска " + today.Format("02.01.2006 15:04")
-	file, err := os.Create("log.txt")
+	str := "Программа была запущена " + today.Format("02.01.2006 15:04") + "\n" + oldData
+
+	file, err := os.Create(filePath)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
+
 	_, err = file.WriteString(str)
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer file.Close()
-
-
 }
